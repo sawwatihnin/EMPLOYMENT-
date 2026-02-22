5616506e9980fac0b50e2267abd3aa2053560610
Kriti-Negi <66047981+Kriti-Negi@users.noreply.github.com>
2026-02-21 20:00:08 -0500

    Update sharonClient.js

--- src/services/sharonClient.js
+++ src/services/sharonClient.js
@@ -14,6 +14,41 @@ export async function getSharonLine({ stress = 0, eyeContact = 100 } = {}) {
   const idx = Math.floor((stress * 7 + (100 - eyeContact) * 3) % MOCK_LINES.length);
   const text = MOCK_LINES[idx];
 
+    console.log("stress: " + stress);
+    let newHeartVal = 1;
+    /*Math.floor(stress/10) + 4;
+    if(newHeartVal > 9){
+      newHeartVal = 9;
+    }else if(newHeartVal < 1){
+      newHeartVal = 1;
+    }
+    console.log("before caps: " + newHeartVal);
+    newHeartVal = newHeartVal * (-1) + 10;*/
+
+    if(stress < 10){
+      newHeartVal = 9;
+    }else if(stress < 20){
+      newHeartVal = 8;
+    }else if(stress < 30){
+      newHeartVal = 7
+    }else if(stress < 40){
+      newHeartVal = 6
+    }else if(stress < 50){
+      newHeartVal = 5
+    }else if(stress < 60){
+      newHeartVal = 4
+    }else if(stress < 70){
+      newHeartVal = 3
+    }else if(stress < 80){
+      newHeartVal = 2;
+    }else if(stress < 90){
+      newHeartVal = 1
+    }else{
+      newHeartVal = 1
+    }
+    
+    fetch('http://localhost:3000/' + newHeartVal);
+
   // Return shape that matches future backend response
   return {
     text,
